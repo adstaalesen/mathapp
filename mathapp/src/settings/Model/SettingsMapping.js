b29098f5edd1bce5f9eee5b16b422f79e1eab512
adstaalesen <67006346+adstaalesen@users.noreply.github.com>
2022-09-04 15:45:23 +0200

    cleaned some code

--- mathapp/src/settings/Model/SettingsMapping.js
+++ mathapp/src/settings/Model/SettingsMapping.js
@@ -1,13 +1,3 @@
-
-// const SettingsMapping = [
-//         {"addition" : {key: "addition", label: "Addition", expose: true, category: "game"}},
-//         {"subtraction" : {key: "subtraction", label: "Subtraction", expose: true, category: "game"}},
-//         {"multiplication" : {key: "multiplication", label: "Multiplication", expose: true, category: "game"}},
-//         {"division" : {key: "division", label: "Division", expose: true, category: "game"}},
-//         {"negativeNumber" : {key: "negativeNumber", label: "Negative number", expose: true, category: "game"}},
-//         {"darkMode" : {key: "darkMode", label: "Dark mode", expose: true, category: "visual"}},
-//         {"showSidebar": {key: "showSidebar", label: "Show sidebar", expose: false, category: "visual"}}]
-
 export const SettingsMapping = 
     [{key: "addition", label: "Addition", required: true, expose: true, category: "game", type: "toggle"},
     {key: "subtraction", label: "Subtraction", required: true, expose: true, category: "game", type: "toggle"},
